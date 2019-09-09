d99ecbc33e348f26c0640366bfb9920fce05004d
Jandeilson De Sousa <jandeilson.desousa@gmail.com>
2019-09-09 00:28:40 -0300

    server

---
+++ config/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const paths = require('./paths');
+
+app.use(express.static(path.join(paths.appBuild)));
+
+app.get('/', function(req, res) {
+    res.sendFile('index.html', { root: path.join(paths.appBuild)});
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
