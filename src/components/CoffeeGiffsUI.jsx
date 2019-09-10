be3d6a7453ef8e8c3bc53e2fa127146fd36608e3
Jandeilson De Sousa <jandeilson.desousa@gmail.com>
2019-09-10 00:56:42 -0300

    loading and css fix

--- src/components/CoffeeGiffsUI.jsx
+++ src/components/CoffeeGiffsUI.jsx
@@ -4,7 +4,17 @@ const CoffeeGifsUI = ({data, change, onLoad}) => {
 
     return (
         <div className="coffeeGifs has-text-centered">
-            <div className="gifLoading" style={data.loading ? {display: 'none'} : {display: 'block'}}/>
+            <div className="sk-cube-grid"  style={data.loading ? {display: 'none'} : {display: 'block'}}>
+                <div className="sk-cube sk-cube1"/>
+                <div className="sk-cube sk-cube2"/>
+                <div className="sk-cube sk-cube3"/>
+                <div className="sk-cube sk-cube4"/>
+                <div className="sk-cube sk-cube5"/>
+                <div className="sk-cube sk-cube6"/>
+                <div className="sk-cube sk-cube7"/>
+                <div className="sk-cube sk-cube8"/>
+                <div className="sk-cube sk-cube9"/>
+            </div>
             <img className="giphyImage" style={data.loading ? {} : {visibility: 'hidden'}} src={data.giphyImage} title={data.giphyTitle} alt={data.giphyTitle} onLoad={onLoad}/>
             <button className="byGiphy" onClick={change}></button>
         </div>
